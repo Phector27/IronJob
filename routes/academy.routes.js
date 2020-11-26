72fd1d28119491e6b4838a3d1f35bb438661425f
Héctor Carramiñana <hector.crm.hcca@gmail.com>
2020-11-26 10:37:21 +0100

    auth routes act

--- routes/academy.routes.js
+++ routes/academy.routes.js
@@ -2,70 +2,14 @@
 
 const express = require('express')
 const router = express.Router()
-const passport = require('passport')
 
 const User = require('../models/user.model')
 const Offer = require('../models/offer.model')
 
-const bcrypt = require('bcryptjs')
-const bcryptSalt = 10
-
 const isLogged = (req, res, next) => req.isAuthenticated() ? next() : res.render('academy/academy-login', { errorMsg: 'Acceso denegado. Haz login para acceder a esta zona de la web.' })
 const checkRole = admittedRoles => (req, res, next) => admittedRoles.includes(req.user.role) ? next() : res.render('academy/academy-login', { errorMsg: 'Acceso denegado. No tienes permisos para ver esta zona de la web. Por favor, contacta con un administrador de IronHack para que modifique tus permisos.' })
 
-
-// Signup Form
-router.get('/signup', (req, res, next) => res.render('academy/academy-signup'))
-
-// Signup Form Management
-router.post('/signup', (req, res, next) => {
-
-    const { username, password } = req.body
-
-    if (!username || !password) {
-        res.render('academy/academy-signup', { errorMsg: 'Por favor, rellena todos los campos' })
-        return
-    }
-
-    User
-        .findOne({ username })
-        .then(theUser => {
-            if (theUser) {
-                res.render('academy/academy-signup', { errorMsg: 'Nombre de usuario ya registrado.' })
-                return
-            }
-
-            const salt = bcrypt.genSaltSync(bcryptSalt)
-            const hashPass = bcrypt.hashSync(password, salt)
-
-            User
-                .create({ username, password: hashPass })
-                .then(() => res.redirect('/academy/login'))
-                .catch(() => res.render('academy/academy-signup', { errorMsg: 'Error. Contacta con el administrador de la web.' }))
-        })
-        .catch(err => next(err))
-})
-
-
-// Login Form
-router.get('/login', (req, res, next) => res.render('academy/academy-login', { errorMsg: req.flash('error') }))
-
-// Login Form Management
-router.post('/login', passport.authenticate('local', {
-    successRedirect: '/academy/private-academy', //are-privada/perfil
-    failureRedirect: '/academy/login',
-    failureFlash: true,
-    passReqToCallback: true
-}))
-
-// Logout
-router.get('/logout', (req, res) => {
-    req.logout()
-    res.redirect('/academy/login')
-})
-
 // Private Area , 'IRONHACK-RECRUITER'.
-
 // Profile-Private Area for IRONHACK
 router.get('/private-academy', isLogged, checkRole(['IRONHACK-RECRUITER']), (req, res, next) => {
 
